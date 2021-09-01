88a22914a087a388564aa8cd6a8a2046fafc059a
ravitejac6 <ravitejapenugonda.3@gmail.com>
2021-09-01 14:33:00 +0530

    feat: search-artists Pipe added
    - In search, if we type those chars included items shown

--- src/app/search-artists.pipe.ts
+++ src/app/search-artists.pipe.ts
@@ -1,12 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'searchArtists'
+  name: "searchArtists",
 })
 export class SearchArtistsPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  transform(pipeData, pipeModifier): any {
+    return pipeData.filter((item) => {
+      return (
+        item["name"].toLowerCase().includes(pipeModifier.toLowerCase()) ||
+        item["reknown"].toLowerCase().includes(pipeModifier.toLowerCase())
+      );
+    });
   }
-
 }
