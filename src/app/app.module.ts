c72559dac578475524c93ee84e1ef8516f5e7a73
ravitejac6 <ravitejapenugonda.3@gmail.com>
2021-09-01 14:45:54 +0530

    fix: searching list display fixed
    - when types and clicks on item, the list won't be displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { ArtistItemComponent } from './artist-item/artist-item.component';
 import { SearchArtistsPipe } from './search-artists.pipe';
+import { ArtistDetailsComponent } from './artist-details/artist-details.component';
 
 @NgModule({
-  declarations: [AppComponent, ArtistItemComponent, SearchArtistsPipe],
+  declarations: [AppComponent, ArtistItemComponent, SearchArtistsPipe, ArtistDetailsComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
